38735640508b4051a3b9e19d21514b2897f87968
Nick Galko <nikolay.galko@gmail.com>
2020-11-01 01:27:16 +0300

    PWA iteration 2.0

--- docs/sw.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-importScripts('sw-toolbox.js');
-self.toolbox.precache(['index.html','css/resume.min.css']);
-self.toolbox.router.get('/images/*', self.toolbox.cacheFirst);
-self.toolbox.router.get('/*', self.toolbox.networkFirst, { networkTimeoutSeconds: 5});
