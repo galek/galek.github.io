df37f6dfc462ad909bbf3f8e8d41c9ca9e3cd265
Nick Galko <nikolay.galko@gmail.com>
2020-11-01 06:35:39 +0300

    Amend

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ toolbox.precache(["index.html", "css/bundle.min.css"]);
 
 // See: https://googlechromelabs.github.io/sw-toolbox/api.html
 toolbox.router.get('js/*', toolbox.cacheFirst);
-toolbox.router.get('vendor/*', toolbox.fastest);
-toolbox.router.get('img/*', toolbox.fastest);
+//toolbox.router.get('vendor/*', toolbox.fastest);
+//toolbox.router.get('img/*', toolbox.fastest);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
     networkTimeoutSeconds: 5
